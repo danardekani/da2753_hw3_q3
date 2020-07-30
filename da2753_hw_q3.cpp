0e2a4e5e40976ff81855b80fa33e120aecfa781c
Daniel <ardekanicapital@gmail.com>
2020-07-30 14:45:47 -0400

    ask user for three integers

--- da2753_hw_q3.cpp
+++ da2753_hw_q3.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world";
+    int a, b, c;
+
+    cout << "Please enter value for a: ";
+    cin >> a;
+    cout << "Please enter value for b: ";
+    cin >> b;
+    cout << "Please enter value for c: ";
+    cin >> c;
+    cout << "This equation has a single real solution x = " << "**placeholder**";
+
     return 0;
 }
