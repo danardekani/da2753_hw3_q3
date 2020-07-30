e25752f63325800b2b3f13724ec1d0314d4a3bba
Daniel <ardekanicapital@gmail.com>
2020-07-30 16:27:52 -0400

    condense code, small changes.

--- da2753_hw_q3.cpp
+++ da2753_hw_q3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    double a, b, c, x1, x2, discriminant, realPart, ImaginaryPart ;
+    double a, b, c, x1, x2, discriminant, realPart, imaginaryPart ;
 
     cout << "Please enter value for a: ";
     cin >> a;
@@ -16,23 +16,27 @@ int main()
     discriminant = b * b - 4 * a * c;
     cout << discriminant << endl;
 
+    // If/else statements are needed to figure out the nature of the real root.
+    // There are three potential solutions based on whether or not the discriminant is <, ==, or > than 0
     if (discriminant > 0)
     {
         x1 = (- b + sqrt(discriminant)) / (2 * a);
         x2 = (- b - sqrt(discriminant)) / (2 * a);
-        cout << "The discriminant is greater than 0, the roots are real and different." << endl;
-        cout << x1 << endl;
-        cout << x2 << endl;
+        cout << "This equation has two real solutions x1 = " << x1 << " and x2 = " << x2 << endl;
     } else if (discriminant == 0)
     {
         x1 = ( - b + sqrt(discriminant)) / (2 * a);
-        cout << "The discriminant is equal to 0, the roots are real and equal" << endl;
-        cout << "x1 = x2 = " << x1 << endl;
+        cout << "This equation has a single real solution x= " << x1 << endl;
+    } else
+    {
+        realPart = - b / (2 * a);
+
+        //the discriminant must be negative to produce an imaginary number
+        imaginaryPart = sqrt(- discriminant) / (2 * a);
+        cout << "This equation has two complex solutions: ";
+        cout << "x1 = " << realPart << " + " << imaginaryPart << " and ";
+        cout << "x2 = " << realPart << " - " << imaginaryPart << endl;
     }
-//    else (discriminant < 0)
-//    {
-//
-//    }
 
     return 0;
 }
