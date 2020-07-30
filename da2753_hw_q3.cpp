80c0c5b96b603c8865cf53369bc1d4b3a9dda44a
Daniel <ardekanicapital@gmail.com>
2020-07-30 15:43:20 -0400

    Isolate discriminant, plug in variables, and create if /else statements to figure out the nature of the root

--- da2753_hw_q3.cpp
+++ da2753_hw_q3.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int a, b, c;
+    double a, b, c, x1, x2, discriminant, realPart, ImaginaryPart ;
 
     cout << "Please enter value for a: ";
     cin >> a;
@@ -11,7 +12,27 @@ int main()
     cin >> b;
     cout << "Please enter value for c: ";
     cin >> c;
-    cout << "This equation has a single real solution x = " << "**placeholder**";
+
+    discriminant = b * b - 4 * a * c;
+    cout << discriminant << endl;
+
+    if (discriminant > 0)
+    {
+        x1 = (- b + sqrt(discriminant)) / (2 * a);
+        x2 = (- b - sqrt(discriminant)) / (2 * a);
+        cout << "The discriminant is greater than 0, the roots are real and different." << endl;
+        cout << x1 << endl;
+        cout << x2 << endl;
+    } else if (discriminant == 0)
+    {
+        x1 = ( - b + sqrt(discriminant)) / (2 * a);
+        cout << "The discriminant is equal to 0, the roots are real and equal" << endl;
+        cout << "x1 = x2 = " << x1 << endl;
+    }
+//    else (discriminant < 0)
+//    {
+//
+//    }
 
     return 0;
 }
